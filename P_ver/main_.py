3afb6e749dc2a14d7a4d9c2ad231ca79ec4e23b0
ikuhiroo <i2ku.nn@gmail.com>
2019-04-12 17:34:46 +0900

    修正

--- P_ver/main_.py
+++ P_ver/main_.py
@@ -71,41 +71,58 @@ def main():
         vec_len = np.array(len(wordVecs[key][0]), dtype=np.int16)
         break
 
-    print("\n----------------")
-    print("word_num: {}".format(len(wordVecs.keys())))
-    print("Input vector length: {}".format(vec_len))
-    print("Output vector length:: {}".format(vec_len * factor))
-    print("L2 Reg(Dict): {}".format(l2_reg))
-    print("L1 Reg(Atom): {}".format(l1_reg))
-    print("----------------\n")
+    # print("\n----------------")
+    # print("word_num: {}".format(len(wordVecs.keys())))
+    # print("Input vector length: {}".format(vec_len))
+    # print("Output vector length:: {}".format(vec_len * factor))
+    # print("L2 Reg(Dict): {}".format(l2_reg))
+    # print("L1 Reg(Atom): {}".format(l1_reg))
+    # print("----------------\n")
 
-    # AとDの初期化
-    atom = {}
-    # 3.6MB (32bit, factor=10)増える
-    for key in wordVecs.keys():
-        atom[key] = (
-            0.6
-            * (1 / np.sqrt(factor * vec_len, dtype=np.float16))
-            * np.random.randn(1, factor * vec_len).astype(np.float16)
-        )
+    # # AとDの初期化
+    # atom = {}
+    # # 3.6MB (32bit, factor=10)増える
+    # for key in wordVecs.keys():
+    #     atom[key] = (
+    #         0.6
+    #         * (1 / np.sqrt(factor * vec_len, dtype=np.float16))
+    #         * np.random.randn(1, factor * vec_len).astype(np.float16)
+    #     )
 
-    # 3.6MB (32bit, factor=10)増える
-    Dict = (
-        0.6
-        * (1 / np.sqrt(vec_len + factor * vec_len).astype(np.float16))
-        * np.random.randn(vec_len, factor * vec_len).astype(np.float16)
+    # # 3.6MB (32bit, factor=10)増える
+    # Dict = (
+    #     0.6
+    #     * (1 / np.sqrt(vec_len + factor * vec_len).astype(np.float16))
+    #     * np.random.randn(vec_len, factor * vec_len).astype(np.float16)
+    # )
+
+    # 学習済みモデルで初期化
+    atom = joblib.load(
+        "./trained_model/numIter = 10_l1_reg = 0.5_l2_reg = 1e-5_factor = 3_rate = 0.05_learning rate/newvec_7.pkl"
     )
 
-    # atom = joblib.load(
-    #     '/Users/1-10robotics/Desktop/Sparse_Overcomplete/P_ver/trained_model/numIter = 6_l1_reg = 0.5_l2_reg = 1e-5_factor = 3_rate = 0.05_learning rate/newvec_5.pkl')
-
-    # Dict = joblib.load(
-    #     '/Users/1-10robotics/Desktop/Sparse_Overcomplete/P_ver/trained_model/numIter = 6_l1_reg = 0.5_l2_reg = 1e-5_factor = 3_rate = 0.05_learning rate/_dict_5.pkl')
+    Dict = joblib.load(
+        "./trained_model/numIter = 10_l1_reg = 0.5_l2_reg = 1e-5_factor = 3_rate = 0.05_learning rate/_dict_7.pkl"
+    )
 
     # Optimizerの初期化
     #  L*V*1 + L*K*3 + V*K*2
     # 90000 + 2700000 + 1800000 = 18.36MB
     Optimizer = param.Param(atom, Dict, vocab_len, vec_len)
+    # 学習済みモデルで初期化
+    Optimizer._del_grad_D = joblib.load(
+        "./trained_model/numIter = 10_l1_reg = 0.5_l2_reg = 1e-5_factor = 3_rate = 0.05_learning rate/_del_grad_D7.pkl"
+    )
+    Optimizer._grad_sum_D = joblib.load(
+        "./trained_model/numIter = 10_l1_reg = 0.5_l2_reg = 1e-5_factor = 3_rate = 0.05_learning rate/_grad_sum_D7.pkl"
+    )
+    Optimizer._del_grad_A = joblib.load(
+        "./trained_model/numIter = 10_l1_reg = 0.5_l2_reg = 1e-5_factor = 3_rate = 0.05_learning rate/_del_grad_A7.pkl"
+    )
+    Optimizer._grad_sum_A = joblib.load(
+        "./trained_model/numIter = 10_l1_reg = 0.5_l2_reg = 1e-5_factor = 3_rate = 0.05_learning rate/_grad_sum_A7.pkl"
+    )
+
     for time in range(1, numIter):
         num_words = 0  # 更新単語数
         # total_error = np.array(0, dtype=np.float16)  # 総ロス
@@ -188,19 +205,116 @@ def main():
                     else:
                         break
 
-            # sparse rate > 90を求める
-            memo = []
+            # 次元ごとに0占有率に関するリストを作成する
+            memo_80 = []
+            memo_80_85 = []
+            memo_85_90 = []
+            memo_90_95 = []
+            memo_95 = []
+            memo_96 = []
+            memo_97 = []
+            memo_98 = []
+            memo_99 = []
+            memo_other = []
             for i in range(len(list(var_per_dimention.values()))):
                 x = list(var_per_dimention.values())[i]
-                if (x.count(0) / len(x)) * 100 > 90 and (
+                if (x.count(0) / len(x)) * 100 < 80:
+                    memo_80.append(i)
+                elif (x.count(0) / len(x)) * 100 >= 80 and (
+                    x.count(0) / len(x)
+                ) * 100 < 85:
+                    memo_80_85.append(i)
+                elif (x.count(0) / len(x)) * 100 >= 85 and (
+                    x.count(0) / len(x)
+                ) * 100 < 90:
+                    memo_85_90.append(i)
+                elif (x.count(0) / len(x)) * 100 <= 90 and (
                     x.count(0) / len(x)
                 ) * 100 < 95:
-                    memo.append(i)
-            logger.info("len(memo): {}".format(len(memo)))
-            logger.info("memo: {}".format(memo))
+                    memo_90_95.append(i)
+                elif (x.count(0) / len(x)) * 100 == 95:
+                    memo_95.append(i)
+                elif (x.count(0) / len(x)) * 100 == 96:
+                    memo_96.append(i)
+                elif (x.count(0) / len(x)) * 100 == 97:
+                    memo_97.append(i)
+                elif (x.count(0) / len(x)) * 100 == 98:
+                    memo_98.append(i)
+                elif (x.count(0) / len(x)) * 100 == 99:
+                    memo_99.append(i)
+                else:
+                    memo_other.append(i)
 
+            logger.info("~80: {}".format(len(memo_80)))
+            logger.info("80~85: {}".format(len(memo_80_85)))
+            logger.info("85~90: {}".format(len(memo_85_90)))
+            logger.info("90~95: {}".format(len(memo_90_95)))
+            logger.info("95: {}".format(len(memo_95)))
+            logger.info("96: {}".format(len(memo_96)))
+            logger.info("97: {}".format(len(memo_97)))
+            logger.info("98: {}".format(len(memo_98)))
+            logger.info("99: {}".format(len(memo_99)))
+            logger.info("other: {}".format(len(memo_other)))
+
+            try:
+                # 特定の次元の平均を求める
+                for d in memo_80:
+                    # i番目の次元のベクトルに注目
+                    x = list(var_per_dimention.values())[d]
+                    # 平均を求める
+                    mean_x = np.average(x)
+                    logger.info("d: {}, mean: {}\n".format(d, mean_x))
+
+                    # 求めた平均から各単語の分散を求める
+                    var_x = [math.sqrt((mean_x - x[i]) ** 2) for i in range(len(x))]
+
+                    # 分散の大きさ順にソートし，indexを返す
+                    index_x_sorted = sorted(
+                        range(len(var_x)), key=lambda k: var_x[k], reverse=True
+                    )
+
+                    # indexの上位5個
+                    target = index_x_sorted[:10]
+
+                    # indexの上位5個の単語を返す
+                    logger.info(
+                        "d: {}, target_word: {}\n".format(d, f_word.iloc[target])
+                    )
+            except:
+                pass
+
+            try:
+                # 特定の次元の平均を求める
+                for d in memo_95:
+                    # i番目の次元のベクトルに注目
+                    x = list(var_per_dimention.values())[d]
+                    # 平均を求める
+                    mean_x = np.average(x)
+                    logger.info("d: {}, mean: {}\n".format(d, mean_x))
+
+                    # 求めた平均から各単語の分散を求める
+                    var_x = [math.sqrt((mean_x - x[i]) ** 2) for i in range(len(x))]
+
+                    # 分散の大きさ順にソートし，indexを返す
+                    index_x_sorted = sorted(
+                        range(len(var_x)), key=lambda k: var_x[k], reverse=True
+                    )
+
+                    logger.info(
+                        "d: {}, max: {}\n".format(d, sorted(var_x, reverse=True)[0])
+                    )
+
+                    # indexの上位5個
+                    target = index_x_sorted[:10]
+
+                    # indexの上位5個の単語を返す
+                    logger.info(
+                        "d: {}, target_word: {}\n".format(d, f_word.iloc[target])
+                    )
+            except:
+                pass
             # 特定の次元の平均を求める
-            for d in memo:
+            for d in memo_99:
                 # i番目の次元のベクトルに注目
                 x = list(var_per_dimention.values())[d]
                 # 平均を求める
@@ -215,13 +329,8 @@ def main():
                     range(len(var_x)), key=lambda k: var_x[k], reverse=True
                 )
 
-                logger.info(
-                    "d: {}, sorted_value: {}\n".format(d, sorted(var_x, reverse=True))
-                )
-
                 # indexの上位5個
                 target = index_x_sorted[:10]
-                logger.info("d: {}, target_index: {}\n".format(d, target))
 
                 # indexの上位5個の単語を返す
                 logger.info("d: {}, target_word: {}\n".format(d, f_word.iloc[target]))
